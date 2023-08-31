2030efd1bfaef148bb845d26f62882ac2fa11de9
rudranath123 <133906642+rudranath123@users.noreply.github.com>
2023-08-31 23:51:32 +0600

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,19 @@
+$(function() {
+
+    $(".popBtn").click(function () {
+
+        $(".popup").addClass('show')
+
+    });
+
+
+    // CROSSBTN
+
+
+    $(".crossBtn").click(function () {
+        $('.popup').removeClass('show')
+    })
+
+
+
+})
